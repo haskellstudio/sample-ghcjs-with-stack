2dd83c1e5285c9056e2bc9e6e4d83c28c19d50b6
haskellstudio <haskellstudio@gmail.com>
2018-04-04 19:14:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ stack build --stack-yaml=stack.yaml
 # Moving the generated files to the js folder:
 mkdir -p js
 cp -r $(stack path --local-install-root --stack-yaml=stack.yaml)/bin/sample-ghcjs-with-stack-exe.jsexe/* js/
+http-server ./js -a 0.0.0.0 -p 8080 -o
